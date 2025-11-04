03962a3d3811667605cfac116c5e690ad6803a67
unknown <imamaffandi715@gmail.com>
2025-11-05 00:05:11 +0700

    update frontend bug

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_BASE_URL = "http://localhost:5000/api";
-const API_BASE_URL = "http://sd/api";
+const API_BASE_URL = "https://gloam-backend.vercel.app/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
